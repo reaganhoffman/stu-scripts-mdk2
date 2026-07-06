abd8a4601d9bb500973c35254eba19bbd4524a1a
Ethan Jewell <ethanjevvell@gmail.com>
2026-07-05 23:34:08 -0500

    Fix galactic map

---
+++ Scripts/STU Planetary Measurements/Displays/MainDisplay/ProgrammableBlock.cs
@@ -0,0 +1,48 @@
+﻿using VRage.Game.GUI.TextPanel;
+using VRageMath;
+
+namespace IngameScript {
+
+    partial class Program {
+
+        public partial class MainDisplay {
+
+            public static class LargeProgrammableBlock {
+                public static void LargeScreen(MySpriteDrawFrame frame, Vector2 centerPos, float scale = 1f) {
+                    frame.Add(new MySprite() {
+                        Type = SpriteType.TEXTURE,
+                        Alignment = TextAlignment.CENTER,
+                        Data = "SquareSimple",
+                        Position = new Vector2(3f, -110f) * scale + centerPos,
+                        Size = new Vector2(512f, 100f) * scale,
+                        Color = new Color(0, 0, 128, 255),
+                        RotationOrScale = 0f
+                    }); // sprite2
+                    frame.Add(new MySprite() {
+                        Type = SpriteType.TEXT,
+                        Alignment = TextAlignment.LEFT,
+                        Data = "STU Planetary Measurements (V 1.0)",
+                        Position = new Vector2(-238f, -126f) * scale + centerPos,
+                        Color = new Color(255, 255, 255, 255),
+                        FontId = "Debug",
+                        RotationOrScale = 1f * scale
+                    }); // text1
+                    frame.Add(new MySprite() {
+                        Type = SpriteType.TEXT,
+                        Alignment = TextAlignment.LEFT,
+                        Data = $"G_w Vector: ({WorldGravityVector.X.ToString("F2")}, {WorldGravityVector.Y.ToString("F2")}, {WorldGravityVector.Z.ToString("F2")})\n" +
+                               $"M_w: {WorldGravityMagnitude.ToString("F2")}\nM_b: {LocalGravityMagnitude.ToString("F2")}",
+                        Position = new Vector2(-218f, -30f) * scale + centerPos,
+                        Color = new Color(255, 255, 255, 255),
+                        FontId = "Debug",
+                        RotationOrScale = 1f * scale
+                    }); // text2
+                }
+            }
+
+            public static class SmallProgrammableBlock {
+
+            }
+        }
+    }
+}
---
+++ Scripts/STU Planetary Measurements/Program.cs
@@ -0,0 +1,72 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System;
+using VRage.Game.GUI.TextPanel;
+using VRageMath;
+
+namespace IngameScript {
+    partial class Program : MyGridProgram {
+
+        IMyTerminalBlock BaseNode;
+        IMyShipController ControlSeat;
+        MainDisplay PBDisplay;
+
+        public Program() {
+            BaseNode = Me;
+            PBDisplay = new MainDisplay(BaseNode, 0, Echo);
+            ControlSeat = GridTerminalSystem.GetBlockWithName("OBSERVER") as IMyShipController;
+        }
+
+        public void Main() {
+            PBDisplay.Update(ControlSeat);
+        }
+
+        public partial class MainDisplay : STUDisplay {
+
+            Action<MySpriteDrawFrame, Vector2, float> Drawer;
+            STUDisplayDrawMapper MainDisplayMap = new STUDisplayDrawMapper {
+                DisplayDrawMapper = {
+                    {
+                        STUDisplayType.CreateDisplayIdentifier(STUDisplayBlock.LargeProgrammableBlock, STUSubDisplay.LargeDisplay), LargeProgrammableBlock.LargeScreen },
+                    }
+            };
+
+            public static Vector3D WorldGravityVector = new Vector3D();
+            public static Vector3D LocalGravityVector = new Vector3D();
+            public static Vector3D ObserverWorldPosition = new Vector3D();
+            public static double WorldGravityMagnitude = 0;
+            public static double LocalGravityMagnitude = 0;
+            public Action<string> Echo;
+
+            public MainDisplay(IMyTerminalBlock block, int displayIndex, Action<string> echo) : base(block, displayIndex) {
+                Drawer = MainDisplayMap.GetDrawFunction(block, displayIndex);
+                Echo = echo;
+            }
+
+            public void CalculateMagnitude() {
+                // Distance formula in 3D
+                WorldGravityMagnitude = WorldGravityVector.Length();
+                LocalGravityMagnitude = LocalGravityVector.Length();
+                Echo($"G_w_v: \n{WorldGravityVector}");
+                Echo($"G_l_v: \n{LocalGravityVector}");
+                Echo($"W_p: \n {ObserverWorldPosition}");
+            }
+
+            public void Draw() {
+                StartFrame();
+                Drawer.Invoke(CurrentFrame, Viewport.Center, 1f);
+                EndAndPaintFrame();
+            }
+
+            public void Update(IMyShipController observer) {
+                // IMPORTANT: .GetNaturalGravity() returns a Vector3D in terms of the world's coordinate system, not the observer's.
+                WorldGravityVector = observer.GetNaturalGravity();
+                LocalGravityVector = Vector3D.TransformNormal(WorldGravityVector, observer.Orientation);
+                ObserverWorldPosition = observer.GetPosition();
+                CalculateMagnitude();
+                Draw();
+            }
+
+        }
+    }
+
+}
